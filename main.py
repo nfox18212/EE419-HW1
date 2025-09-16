92c4ad9d925bca40f56c876eab418729afe05d84
nfox18212 <nfox18212@gmail.com>
2025-09-16 19:03:07 -0400

    init homework stuff

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import scipy as sp
+import matplotlib as mp
+import sympy 
+
+from scipy import linalg
+from sympy import *
+
+def main():
+    print("Hello from hw1!")
+    A_sym = Matrix([[1,3,5,25],[0,2,4,18],[2,4,6,32]]) 
+
+
+
+if __name__ == "__main__":
+    main()
